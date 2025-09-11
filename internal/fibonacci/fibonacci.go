1a9a5c55ac46ccc7bb5ee2ffeb720a81d2f219da
gustavol007 <gustavogabrielrodrigues123@gmail.com>
2025-09-11 20:06:39 -0300

    Update: Função fibonacci

--- internal/fibonacci/fibonacci.go
+++ internal/fibonacci/fibonacci.go
@@ -1,12 +1,10 @@
 package fibonacci
 
 func Fibonacci(n int) int {
-	if n <= 1 {
-		return n
+	if n <= 0 {
+		return 0
+	} else if n-1 <= 1{
+		return n-1
 	}
-	a, b := 0, 1
-	for i := 2; i <= n; i++ {
-		a, b = b, a+b
-	}
-	return b
+	return Fibonacci(n-1) + Fibonacci(n-2)
 }
